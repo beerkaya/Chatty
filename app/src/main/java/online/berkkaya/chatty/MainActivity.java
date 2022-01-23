897b0bf0f9b2bd6189623dcd7b243a028e51da04
beerkaya <berk-6023@yandex.com>
2022-01-23 19:02:47 +0300

    FriendShip added

--- app/src/main/java/online/berkkaya/chatty/MainActivity.java
+++ app/src/main/java/online/berkkaya/chatty/MainActivity.java
@@ -3,17 +3,37 @@ package online.berkkaya.chatty;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseFirestore db;
+    private FloatingActionButton fabAdd;
+    private String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -31,10 +52,106 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(i);
                     finish();
                 } else {
-
+                    OpenMainActivity();
                 }
             }
         };
+    }
+    private void OpenMainActivity() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        uid = currentUser.getUid();
+
+        fabAdd = findViewById(R.id.fabAdd);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context;
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setTitle("Enter User Uid: ");
+                dialog.setContentView(R.layout.layout_add);
+                dialog.show();
+
+                EditText edtID = dialog.findViewById(R.id.edtId);
+                Button btnOk = dialog.findViewById(R.id.btnOk);
+
+                btnOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String idUser = edtID.getText().toString();
+                        if (TextUtils.isEmpty(idUser)) {
+                            edtID.setError("required");
+                        } else {
+                            db.collection("user").whereEqualTo("id", idUser).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                @Override
+                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                    if (queryDocumentSnapshots.isEmpty()) {
+                                        edtID.setError("Id not found!");
+                                    } else {
+                                        for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()) {
+                                            String uidFriend = documentSnapshot.getId();
+                                            if (uid.equals(uidFriend)) {
+                                                edtID.setError("Wrong ID");
+                                            } else {
+                                                dialog.cancel();
+                                                checkFriendExists(uidFriend);
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void checkFriendExists(String uidFriend) {
+        db.collection("user").document(uid).collection("friend").document().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        String idChatRoom = documentSnapshot.get("idChatRoom", String.class);
+                        goChatRoom(idChatRoom, uidFriend);
+                    } else {
+                        createNewChatRoom(uidFriend);
+                    }
+                }
+            }
+        });
+    }
+
+    private void createNewChatRoom(String uidFriend) {
+        HashMap<String, Object> dataChatRoom = new HashMap<>();
+        dataChatRoom.put("dateAdded", FieldValue.serverTimestamp());
+        db.collection("chatRoom").document(uid+uidFriend).set(dataChatRoom).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                // kullanıcı verisine yazma
+                HashMap<String, Object> dataFriend = new HashMap<>();
+                dataFriend.put("idChatRoom", uid+uidFriend);
+                db.collection("user").document(uid).collection("friend").document(uidFriend).set(dataFriend).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        // kullanıcının arkadaşının verisine yazma
+                        HashMap<String, Object> dataUserFriend = new HashMap<>();
+                        dataUserFriend.put("idChatRoom", uid+uidFriend);
+                        db.collection("user").document(uidFriend).collection("friend").document(uid).set(dataUserFriend).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                goChatRoom(uid+uidFriend, uidFriend);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private void goChatRoom(String idChatRoom, String uidFriend) {
+
     }
 
     @Override
